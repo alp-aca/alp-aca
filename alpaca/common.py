1734c28b7b67a028df935b675d515cc92d5672c4
Xavier Ponce <xavipondi@gmail.com>
2024-10-09 17:47:39 +0200

    All Quarkonia Functions added

--- alpaca/common.py
+++ alpaca/common.py
@@ -24,3 +24,39 @@ alpha_s = lambda q: flavio.physics.running.running.get_alpha_s(pars, q)
 f0_BK = lambda q2: flavio.physics.bdecays.formfactors.b_p.bcl.ff('B->K', q2, pars)['f0']
 f0_Kpi = lambda q2: flavio.physics.kdecays.formfactors.fp0_dispersive(q2, pars)['f0']
 A0_BKst = lambda q2: flavio.physics.bdecays.formfactors.b_v.bsz.ff('B->K*', q2, pars)['A0']
+
+################################################
+############ Quarkonia Parameters ##############
+################################################
+
+Qb=-1/3
+Qc=2/3
+
+mJPSI=3.096
+GammaJpsi=0.0929*10**-3
+fJpsi=0.414
+BeeJPSI=5.971*10**-2
+
+muppsilon1s=9.46030
+BeeUpsilon1s=2.48*10**-3
+fUps1S=0.680
+GammaUps1S=54.02*10**-6
+
+muppsilon3s=10.3552
+GammaUps3S=20.32*10**-6
+BeeUppsilon3s=2.18*10**-2
+fUps3S=0.405
+
+s_BESIII=mJPSI**2
+sigmaW_BESIII=(3.686)*5*10**-4
+
+BabarCM=muppsilon3s**2
+sigmaW_babar=5.5*10**-3
+
+Belle1CM=muppsilon1s**2
+sigmaW_belle1=5.5*10**-3
+
+Belle2CM=10.58
+
+
+
--- alpaca/decays/mesons/invisible.py
+++ alpaca/decays/mesons/invisible.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from ...common import f0_BK, f0_Kpi, A0_BKst, alpha_em, kallen
+from ...common import f0_BK, f0_Kpi, A0_BKst, alpha_em, mJPSI,muppsilon3s, BeeJPSI, BeeUppsilon3s, kallen
 from ...rge import ALPcouplings
 from ...citations import citations
 
@@ -31,3 +31,32 @@ def sigmaNR(ma: float, couplings: ALPcouplings, s: float, f_a: float=1000,**kwar
     coup_low = couplings.match_run(ma, 'kF_below', **kwargs)
     gaphoton = coup_low['cgamma']*alpha_em(np.sqrt(s))/(np.pi*f_a)
     return (((alpha_em(np.sqrt(s))*gaphoton**2)/24)*(1-(ma**2)/s)**3)*0.389379e9
+
+def BR_Vagamma(ma: float, couplings: ALPcouplings, s: float, mV: float, fV: float, Q: float, GammaV: float, f_a: float=1000, **kwargs):
+    citations.register_inspire('Merlo:2019anv')
+    citations.register_inspire('DiLuzio:2024jip')
+    coup_low = couplings.match_run(ma, 'kF_below', **kwargs)
+    if Q == -1/3:
+        gaff = coup_low['kD'][2,2]/f_a
+    elif Q==2/3:
+        gaff = coup_low['kU'][1,1]/f_a
+    else:
+        raise ValueError("Q must be -1/3 or 2/3")
+    gaphoton = coup_low['cgamma']*alpha_em(np.sqrt(mV))/(np.pi*f_a)
+    return ((alpha_em(mV)*(Q**2)*mV*fV**2)/(24*GammaV))*(1-(ma**2)/mV**2)*((gaphoton)*(1-ma**2/mV**2)-2*gaff)**2
+
+def sigmapeak(mV, BeeV):
+     return (12*np.pi*BeeV/(mV**2))*0.389379*10**9
+
+def Mixed_QuarkoniaSearches(ma: float, couplings: ALPcouplings, s: float, mV: float, fV: float, Q: float, GammaV: float, f_a: float=1000, **kwargs):
+    if mV == mJPSI:
+        Corr_Factor=0.03075942
+        sigma_peak=sigmapeak(mV, BeeJPSI)
+    elif mV == muppsilon3s:
+        Corr_Factor=0.0023112
+        sigma_peak=sigmapeak(mV, BeeUppsilon3s)
+    else:
+        raise ValueError("mV must be mJPSI or muppsilon3s")
+    
+    return BR_Vagamma(ma, couplings, s, mV, fV, Q, GammaV, f_a, **kwargs)+sigmaNR(ma, couplings, s, f_a, **kwargs)/(Corr_Factor*sigma_peak)
+        
