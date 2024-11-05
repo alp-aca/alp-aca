fix: cross section units inconsistency

alpaca/constants.py
@@ -132,7 +132,7 @@ e_C = Constant(1.602176634e-19, 'Mohr:2024kco')
 c_nm_per_ps = Constant(299792.458, 'Mohr:2024kco')
 hbar_GeVps = LazyFloat(lambda: h_Js/(e_C*2*np.pi)*1e3)
 hbarc_GeVnm = LazyFloat(lambda: hbar_GeVps*c_nm_per_ps)
-hbarc2_GeV2pb = LazyFloat(lambda: hbarc_GeVnm**(-2)*1e-22)
+hbarc2_GeV2pb = LazyFloat(lambda: hbarc_GeVnm**2*1e22)
 
 # Collider parameters
 sigmaW_BaBar = Constant(5.5e-3, 'Merlo:2019anv') # See footnote in page 5

alpaca/experimental_data/measurements_exp.py
@@ -414,14 +414,14 @@ def besIII_D0topi0nunu(x):
 #BelleII e+e- -> gamma a
     #Experiment: BelleII
     #arXiv: 2007.13071
-    #@90% confidence level
-    #Cross section
+    #@95% confidence level
+    #Cross section (pb)
 def belleII_upsilon4s(x):
     citations.register_inspire('Belle-II:2020jti')
     data_file_path = os.path.join(current_dir, invisible, 'Belle2_gamma_binned.txt')
     q2min, q2max, value = data_reading(data_file_path)
     values, sigmals, sigmars = bin_selection(x, q2min, q2max, value, 0.5*np.array(value), 0.5*np.array(value))
-    return values, sigmals, sigmars
+    return values*1e12, sigmals*1e12, sigmars*1e12
 
 #################################### VISIBLE SEARCHES ####################################
 