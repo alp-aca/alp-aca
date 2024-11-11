9321aaff30798a559003319b21e14dc4d7d615b2
Jorge-Alda <jorge.alda@pd.infn.it>
2024-11-11 17:51:10 +0100

    feat: calculation of chi^2 including decay type probability

--- alpaca/statistics/chisquared.py
+++ alpaca/statistics/chisquared.py
@@ -1,15 +1,16 @@
 import numpy as np
-from .functions import tensor_meshgrid
+from ..decays.alp_decays.branching_ratios import total_decay_width
+from ..decays.decays import branching_ratio
+from ..constants import hbarc_GeVnm
+from ..experimental_data.classes import MeasurementBase
 
-def chi2_obs(observable, measurement, ma, obsargs):
-    expv = np.vectorize(measurement, otypes=[float, float, float])(ma)
-    ma_m, obsargs_m = tensor_meshgrid(ma, obsargs)
-    expmean, _ = tensor_meshgrid(expv[0], obsargs)
-    expuncert, _ = tensor_meshgrid(expv[2], obsargs)
-    valid = np.where(expv[2] != 0)
-    thpred = np.full(obsargs_m.shape, np.nan)
-    thpred[valid,...] = observable(ma_m[valid,...], obsargs_m[valid,...])
-    return (thpred - expmean)**2/expuncert**2
+def chi2_obs(meausrement: MeasurementBase, transition: str, ma, couplings, fa, **kwargs):
+    kwargs_dw = {k: v for k, v in kwargs.items() if k != 'theta'}
+    dw = np.vectorize(lambda ma, coupl, fa: total_decay_width(ma, coupl, fa, **kwargs_dw)['DW_tot'])(ma, couplings, fa)
+    ctau = 1e-7*hbarc_GeVnm/dw
+    prob_decay = meausrement.decay_probability(ctau, ma, theta=kwargs.get('theta', None))
+    br = branching_ratio(transition, ma, couplings, fa, **kwargs_dw)
+    return (meausrement.get_central(ma, ctau) - prob_decay*br)**2/(meausrement.get_sigma_left(ma, ctau)+meausrement.get_sigma_right(ma, ctau))**2
 
 def combine_chi2(*chi2):
     ndof = np.sum(np.where(np.isnan(m), 0, 1) for m in chi2)
