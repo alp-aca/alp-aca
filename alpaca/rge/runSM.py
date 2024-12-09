0da69d3a0409e5d82de465d03872e3cc6e67a1ee
Jorge-Alda <jorge.alda@pd.infn.it>
2024-12-09 19:14:57 +0100

    fix: cache the running of the SM parameters

--- alpaca/rge/runSM.py
+++ alpaca/rge/runSM.py
@@ -3,8 +3,9 @@ import ckmutil
 import numpy as np
 from cmath import phase
 from ..citations import citations
+from functools import lru_cache
 
-def runSM(scale):
+def runSM(scale: float) -> dict:
     """SM parameters at an energy scale
     
     Parameters
@@ -17,6 +18,10 @@ def runSM(scale):
     pars : dict
         dict containing the Yukawa matrices `yu`, `yd` and `ye`, the gauge couplings `alpha_s`, `alpha_1` and `alpha_2`, the sine squared of the Weinberg angle `s2w` and the CKM matrix `CKM`.
     """
+    return _runSM(scale)
+
+@lru_cache
+def _runSM(scale):
     citations.register_inspire('Aebischer:2018bkb') #wilson
     citations.register_inspire('Straub:2018kue') # ckmutil is inside flavio's repo
     wSM = wilson.classes.SMEFT(wilson.wcxf.WC('SMEFT', 'Warsaw', scale, {})).C_in # For the moment we reuse wilson's code for the SM case, i.e, with all Wilson coefficients set to zero. Maybe at some point we should implement our own version.
