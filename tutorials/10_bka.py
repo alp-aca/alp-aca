e5d0981734042b43d892de03b5c3f11b5ae9cef8
Jorge-Alda <jorge.alda@pd.infn.it>
2024-12-17 11:20:46 +0100

    K->pi a results

---
+++ tutorials/10_bka.py
@@ -0,0 +1,48 @@
+from alpaca.statistics.chisquared import get_chi2
+from alpaca.statistics.functions import nsigmas
+from alpaca.models.model_library import QED_DFSZ, beta
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 12, 'text.usetex': True, 'font.family': 'serif', 'font.serif': 'Computer Modern Roman'})
+
+ma = np.linspace(0.103, 5.0, 500)
+fa = np.logspace(3, 8, 500)
+couplings = [QED_DFSZ.get_couplings({beta: np.pi/4}, f).match_run(5.1, 'VA_below', 'leadinglog') for f in fa]
+x_ma, y_couplings = np.meshgrid(ma, couplings)
+x_ma, y_fa = np.meshgrid(ma, fa)
+
+transitions = [
+    'B+ -> K+ a',
+    'B0 -> K*0 a',
+    'B+ -> K+ e e',
+    'B0 -> K*0 e e',
+    'B+ -> K+ mu mu',
+    'B0 -> K*0 mu mu',
+]
+
+chi2 = get_chi2(transitions, x_ma, y_couplings, y_fa, integrator='no_rge')
+
+plt.figure(figsize=(16/2.5, 10/2.5))
+colors = {('B+ -> K+ a', 'Belle II'): 'cyan', ('B0 -> K*0 a', 'BaBar'): 'teal', ('B+ -> K+ e e', 'Belle II'): 'purple', ('B0 -> K*0 e e', 'Belle II'): 'magenta', ('B+ -> K+ mu mu', 'LHCb'): 'limegreen', ('B+ -> K+ mu mu', 'Belle II'): 'yellow', ('B+ -> K+ mu mu', 'CHARM'): 'green', ('B0 -> K*0 mu mu', 'LHCb'): 'red', ('B0 -> K*0 mu mu', 'Belle II'): 'orange'}
+tex = {'B+ -> K+ a': r'$B^+ \to K^+ \nu \nu$', 'B0 -> K*0 a': r'$B^0 \to K^{*0} \nu \nu$', 'B+ -> K+ e e': r'$B^+ \to K^+ e^+ e^-$', 'B0 -> K*0 e e': r'$B^0 \to K^{*0} e^+ e^-$', 'B+ -> K+ mu mu': r'$B^+ \to K^+ \mu^+ \mu^-$', 'B0 -> K*0 mu mu': r'$B^0 \to K^{*0} \mu^+ \mu^-$'}
+legend_elements = []
+pl = plt.contourf(x_ma, 1/y_fa, nsigmas(chi2[('', 'Global')],2), levels=list(np.linspace(0, 3, 100)), cmap='magma_r', vmax=3, extend='max')
+for observable, chi2_obs in chi2.items():
+    if observable == ('', 'Global'):
+        break
+    plt.contour(x_ma, 1/y_fa, nsigmas(chi2_obs, 2), levels=[2], colors = colors[observable])
+    legend_elements.append(plt.Line2D([0], [0], color=colors[observable], label=tex[observable[0]] + ' (' + observable[1] + ')'))
+ax = plt.gca()
+ax.set_xscale('log')
+ax.set_yscale('log')
+cb = plt.colorbar(pl, extend='max')
+cb.set_label(r'Exclusion significance [$\sigma$]')
+cb.set_ticks([0, 1, 2, 3])
+plt.xlabel(r'$m_a$ [GeV]')
+plt.ylabel(r'$f_a^{-1}$ [GeV$^{-1}$]')
+plt.xlim(0.1, 5)
+plt.title(r'$B \to K a$, QED-DFSZ with $\tan\beta = 1$')
+plt.legend(handles = legend_elements, loc='center left', bbox_to_anchor=(1, 0.5), borderaxespad=7, fontsize=8)
+plt.tight_layout(pad=1.5)
+plt.savefig('BtoKa.pdf', dpi = 1000)
