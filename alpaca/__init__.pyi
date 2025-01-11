f8e7703d0a00e2e1559c0e05bc04174026327423
Jorge-Alda <jorge.alda@pd.infn.it>
2025-01-11 02:10:55 +0100

    fix: Lazy-load packages at startup
    Add documentation to functions #12

---
+++ alpaca/__init__.pyi
@@ -0,0 +1,20 @@
+from .rge.classes import (
+    ALPcouplings as ALPcouplings,
+    ALPcouplingsEncoder as ALPcouplingsEncoder,
+    ALPcouplingsDecoder as ALPcouplingsDecoder,
+)
+
+from .decays.decays import (
+    decay_width as decay_width,
+    branching_ratio as branching_ratio,
+    cross_section as cross_section,
+)
+
+from .citations import citations as citations
+
+from . import(
+    models as models,
+    experimental_data as experimental_data,
+    statistics as statistics,
+    plotting as plotting,
+)
