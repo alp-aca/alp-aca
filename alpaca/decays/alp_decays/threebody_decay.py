fix: rotation in Y axis

alpaca/decays/alp_decays/threebody_decay.py
@@ -43,14 +43,14 @@ def kinematics(M,m1,m2,m3,Ener3,theta, thetaast, phiast):   #M,m1,m2,m3,m12,cthe
     q2ast3 = -q12CM* np.cos(thetaast)
 
     q10 = Ener12/m12*(q1ast0+np.sqrt(1-m12**2/Ener12**2)*q1ast3)
-    q11 = -q1ast1*np.cos(theta)-q1ast2*np.sin(theta)
-    q12 = -q1ast2*np.cos(theta)+q1ast1*np.sin(theta)
-    q13 = Ener12/m12*(np.sqrt(1-m12**2/Ener12**2)*q1ast0+q1ast3)
+    q11 = -q1ast1*np.cos(theta)-Ener12/m12*np.sin(theta)*(q1ast0*np.sqrt(1-m12**2/Ener12**2)+q1ast3)
+    q12 = q1ast2
+    q13 = q1ast1*np.sin(theta)-Ener12/m12*np.cos(theta)*(np.sqrt(1-m12**2/Ener12**2)*q1ast0+q1ast3)
 
     q20 = Ener12/m12*(q2ast0+np.sqrt(1-m12**2/Ener12**2)*q2ast3)
-    q21 = -q2ast1*np.cos(theta)-q2ast2*np.sin(theta)
-    q22 = -q2ast2*np.cos(theta)+q2ast1*np.sin(theta)
-    q23 = Ener12/m12*(np.sqrt(1-m12**2/Ener12**2)*q2ast0+q2ast3)
+    q21 = -q2ast1*np.cos(theta)-Ener12/m12*np.sin(theta)*(q2ast0*np.sqrt(1-m12**2/Ener12**2)+q2ast3)
+    q22 = q2ast2
+    q23 = q2ast1*np.sin(theta)-Ener12/m12*np.cos(theta)*(np.sqrt(1-m12**2/Ener12**2)*q2ast0+q2ast3)
 
     
     p0p0 = M**2