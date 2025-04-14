fix: a->3pi0 consistent calculation

alpaca/chiPT/chiral.py
@@ -12,8 +12,8 @@ import os
 #with open(os.path.join(path, 'ffunction.pickle'), 'rb') as f:
 #    ffunction_interp = pickle.load(f)
 
-#kappa = np.diag([1/m for m in [mu, md, ms]])/sum(1/m for m in [mu, md, ms])
-kappa = np.diag([0,1,0])
+kappa = np.diag([1/m for m in [mu, md, ms]])/sum(1/m for m in [mu, md, ms])
+#kappa = np.diag([0,1,0])
 
 def cqhat(couplings: ALPcouplings, ma: float, **kwargs) -> np.ndarray:
     cc = couplings.match_run(ma, 'VA_below', **kwargs)
@@ -41,7 +41,7 @@ def kinetic_mixing(ma: float, couplings: ALPcouplings, fa: float, **kwargs) -> d
     return {
         'pi0': eps*(cq_eff[0,0]-cq_eff[1,1])/2,
         'eta': eps*np.sqrt(3)/6*((cq_eff[0,0]+cq_eff[1,1])*(c_eta-np.sqrt(2)*s_eta) - cq_eff[2,2]*(2*c_eta+np.sqrt(2)*s_eta)),
-        'etap': eps/2/np.sqrt(3)*((np.sqrt(2)*c_eta + s_eta)*(cq_eff[0,0]+cq_eff[1,1]) + cq_eff[2,2]*(np.sqrt(2)*c_eta-2*np.sqrt(2)*s_eta)),
+        'etap': eps/2/np.sqrt(3)*((np.sqrt(2)*c_eta + s_eta)*(cq_eff[0,0]+cq_eff[1,1]) + cq_eff[2,2]*(np.sqrt(2)*c_eta-2*s_eta)),
         'K0': eps/np.sqrt(2)*cq_eff[1,2],
         'K0bar': eps/np.sqrt(2)*cq_eff[2,1],
         }
@@ -73,7 +73,9 @@ def sm_massmixing():
 def sm_mixingangles():
     res = dict()
     sm_mixing = sm_massmixing()
-    masses = mesonmass_chiPT()
+    #masses = mesonmass_chiPT()
+    from ..constants import mpi0, meta, metap, mK0
+    masses = {'pi0': mpi0, 'eta': meta, 'etap': metap, 'K0': mK0, 'K0bar': mK0}
     for m1 in ('pi0', 'eta', 'etap'):
         for m2 in ('pi0', 'eta', 'etap'):
             if m1 == m2:
@@ -89,14 +91,16 @@ def a_U3_repr(ma: float, couplings: ALPcouplings, fa: float, **kwargs) -> np.nda
     alp_mixing = dict()
     kmix = kinetic_mixing(ma, couplings, fa, **kwargs)
     mmix = mass_mixing(ma, couplings, fa, **kwargs)
-    mesonmass = mesonmass_chiPT()
+    #mesonmass = mesonmass_chiPT()
+    from ..constants import mpi0, meta, metap, mK0
+    mesonmass = {'pi0': mpi0, 'eta': meta, 'etap': metap, 'K0': mK0, 'K0bar': mK0}
     for m1 in mesons:
         h = mmix[m1] - ma**2 * kmix[m1]
         for m2 in mesons:
             h += sm_mixing.get((m1, m2), 0) * (mmix[m2] - ma**2 * kmix[m2])/(ma**2-mesonmass[m2]**2)
         h /= (ma**2-mesonmass[m1]**2)
         alp_mixing[m1] = h
-    return (u3reprs.pi0 * alp_mixing['pi0'] + u3reprs.eta * alp_mixing['eta'] + u3reprs.etap * alp_mixing['etap'] + u3reprs.K0 * alp_mixing['K0'] + u3reprs.K0bar * alp_mixing['K0bar'])*ffunction(ma)**2
+    return (u3reprs.pi0 * alp_mixing['pi0'] + u3reprs.eta * alp_mixing['eta'] + u3reprs.etap * alp_mixing['etap'] + u3reprs.K0 * alp_mixing['K0'] + u3reprs.K0bar * alp_mixing['K0bar'])
 
 def alphas_tilde(ma: float) -> float:
     if ma < 1.0:
@@ -128,8 +132,8 @@ class _ffunction:
         citations.register_inspire('BaBar:2017zmc')
         citations.register_inspire('BaBar:2007ceh')
         citations.register_inspire('BaBar:2004ytv')
-        if ma < 1.4: fun = 1
-        elif ma >= 1.4 and ma <= 2: 
+        if ma < 1.45: fun = 1
+        elif ma >= 1.45 and ma <= 2: 
             fun = self.ffunction_interp(ma)
         else: fun = (1.4/ma)**4
         return fun

alpaca/decays/alp_decays/threebody_decay.py
@@ -42,15 +42,18 @@ def kinematics(M,m1,m2,m3,Ener3,theta, thetaast, phiast):   #M,m1,m2,m3,m12,cthe
     q2ast2 = -q12CM* np.sin(thetaast)*np.sin(phiast)
     q2ast3 = -q12CM* np.cos(thetaast)
 
-    q10 = Ener12/m12*(q1ast0+np.sqrt(1-m12**2/Ener12**2)*q1ast3)
-    q11 = -q1ast1*np.cos(theta)-q1ast2*np.sin(theta)
-    q12 = -q1ast2*np.cos(theta)+q1ast1*np.sin(theta)
-    q13 = Ener12/m12*(np.sqrt(1-m12**2/Ener12**2)*q1ast0+q1ast3)
+    gamma = Ener12/m12
+    gamma_beta = np.sqrt(gamma**2-1)
 
-    q20 = Ener12/m12*(q2ast0+np.sqrt(1-m12**2/Ener12**2)*q2ast3)
-    q21 = -q2ast1*np.cos(theta)-q2ast2*np.sin(theta)
-    q22 = -q2ast2*np.cos(theta)+q2ast1*np.sin(theta)
-    q23 = Ener12/m12*(np.sqrt(1-m12**2/Ener12**2)*q2ast0+q2ast3)
+    q10 = gamma * q1ast0 + gamma_beta * q1ast3
+    q11 = -q1ast1*np.cos(theta)-(gamma_beta * q1ast0 + gamma * q1ast3)*np.sin(theta)
+    q12 = q1ast2
+    q13 = q1ast1*np.sin(theta)-(gamma_beta * q1ast0 + gamma * q1ast3)*np.cos(theta)
+
+    q20 = gamma * q2ast0 + gamma_beta * q2ast3
+    q21 = -q2ast1*np.cos(theta)-(gamma_beta * q2ast0 + gamma * q2ast3)*np.sin(theta)
+    q22 = q2ast2
+    q23 = q2ast1*np.sin(theta)-(gamma_beta * q2ast0 + gamma * q2ast3)*np.cos(theta)
 
     
     p0p0 = M**2