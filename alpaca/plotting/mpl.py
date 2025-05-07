289c8ffe03355b5500b4b7ccaa8231964e622a8d
Jorge-Alda <jorge.alda@pd.infn.it>
2025-05-07 14:26:35 +0200

    feat: new palette for exclusion plot

--- alpaca/plotting/mpl.py
+++ alpaca/plotting/mpl.py
@@ -3,7 +3,7 @@ from matplotlib.colors import ListedColormap
 plt.rcParams.update({'font.size': 12, 'text.usetex': True, 'font.family': 'serif', 'font.serif': 'Computer Modern Roman'})
 import numpy as np
 from ..statistics.functions import nsigmas
-from .palettes import darker_set3, newmagma
+from .palettes import darker_set3, trafficlights
 
 def exclusionplot(x, y, chi2, xlabel, ylabel, title, tex, ax=None):
     def max_finite(x):
@@ -11,7 +11,7 @@ def exclusionplot(x, y, chi2, xlabel, ylabel, title, tex, ax=None):
         if len(finite) == 0:
             return -1
         return np.max(finite)
-    cmap_newmaga = ListedColormap(newmagma+['#000000'])
+    cmap_trafficlights = ListedColormap(trafficlights+['#000000'])
     colors = {k: c for k, c in zip(chi2.keys(), darker_set3*4)}
     styles_list = ['solid']*len(darker_set3) + ['dashed']*len(darker_set3) + ['dotted']*len(darker_set3) + ['dashdot']*len(darker_set3)
     lss = {k: s for k, s in zip(chi2.keys(), styles_list)}
@@ -19,7 +19,7 @@ def exclusionplot(x, y, chi2, xlabel, ylabel, title, tex, ax=None):
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1, xticks=[], yticks=[])
     legend_elements = []
-    pl = plt.contourf(x,y, nsigmas(chi2[('', 'Global')][0],chi2[('', 'Global')][1]), levels=list(np.linspace(0, 3, 100)), cmap=cmap_newmaga, vmax=3, extend='max')
+    pl = plt.contourf(x,y, nsigmas(chi2[('', 'Global')][0],chi2[('', 'Global')][1]), levels=list(np.linspace(0, 5, 150)), cmap=cmap_trafficlights, vmax=5, extend='max')
     
     for observable, chi2_obs in chi2.items():
         if observable == ('', 'Global'):
@@ -38,7 +38,7 @@ def exclusionplot(x, y, chi2, xlabel, ylabel, title, tex, ax=None):
     ax.set_yscale('log')
     cb = plt.colorbar(pl, extend='max')
     cb.set_label(r'Exclusion significance [$\sigma$]')
-    cb.set_ticks([0, 1, 2, 3])
+    cb.set_ticks([0, 1, 2, 3, 4, 5])
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title, fontsize=12)
