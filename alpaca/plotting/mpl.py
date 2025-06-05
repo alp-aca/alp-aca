66ad559321d408e90363e0499a48e9cebcbe43be
Jorge-Alda <jorge.alda@pd.infn.it>
2025-06-05 18:09:18 +0200

    feat: plot a chi2 global different from the combined

--- alpaca/plotting/mpl.py
+++ alpaca/plotting/mpl.py
@@ -6,7 +6,30 @@ from ..statistics.functions import nsigmas
 from .palettes import darker_set3, trafficlights
 from ..statistics.chisquared import ChiSquared, combine_chi2
 
-def exclusionplot(x: np.ndarray[float], y: np.ndarray[float], chi2: list[ChiSquared], xlabel: str, ylabel: str, title: str, ax=None):
+def exclusionplot(x: np.ndarray[float], y: np.ndarray[float], chi2: list[ChiSquared], xlabel: str, ylabel: str, title: str | None = None, ax: plt.Axes | None = None, global_chi2: ChiSquared | None = None) -> plt.Axes:
+    """
+    Create an exclusion plot.
+
+    Parameters
+    ----------
+    x : np.ndarray[float]
+        The x-coordinates of the data points.
+    y : np.ndarray[float]
+        The y-coordinates of the data points.
+    chi2 : list[ChiSquared]
+        A list of ChiSquared objects representing the exclusion regions.
+    xlabel : str
+        The label for the x-axis.
+    ylabel : str
+        The label for the y-axis.
+    title : str | None, optional
+        The title of the plot (default is None).
+    ax : plt.Axes | None, optional
+        The matplotlib Axes object to plot on (default is None, which creates a new figure).
+    global_chi2 : ChiSquared | None, optional
+        A ChiSquared object representing the global exclusion significance (default is None, which uses the combined chi squared).
+
+    """
     cmap_trafficlights = ListedColormap(trafficlights+['#000000'])
     colors = darker_set3*4
     lss = ['solid']*len(darker_set3) + ['dashed']*len(darker_set3) + ['dotted']*len(darker_set3) + ['dashdot']*len(darker_set3)
@@ -14,7 +37,8 @@ def exclusionplot(x: np.ndarray[float], y: np.ndarray[float], chi2: list[ChiSqua
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1, xticks=[], yticks=[])
     legend_elements = []
-    global_chi2 = combine_chi2(chi2, 'Global', 'Global', 'Global')
+    if global_chi2 is None:
+        global_chi2 = combine_chi2(chi2, 'Global', 'Global', 'Global')
     pl = plt.contourf(x,y, global_chi2.significance(), levels=list(np.linspace(0, 5, 150)), cmap=cmap_trafficlights, vmax=5, extend='max', zorder=-20)
     
     i_color = 0
@@ -49,7 +73,8 @@ def exclusionplot(x: np.ndarray[float], y: np.ndarray[float], chi2: list[ChiSqua
     cb.set_ticks([0, 1, 2, 3, 4, 5])
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    plt.title(title, fontsize=12)
+    if title is not None:
+        plt.title(title, fontsize=12)
     plt.legend(handles = legend_elements, loc='center left', bbox_to_anchor=(1, 0.5), borderaxespad=9, fontsize=8)
     plt.tight_layout()
 
