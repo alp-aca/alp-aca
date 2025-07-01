03b6592c00af038be5d292df8fe525d6fdc6b81f
Jorge-Alda <jorge.alda@pd.infn.it>
2025-07-01 14:54:29 +0200

    feat: add ChiSquared and ChiSquaredList to alpaca.statistics namespace

--- alpaca/statistics/__init__.pyi
+++ alpaca/statistics/__init__.pyi
@@ -1,6 +1,8 @@
 from .chisquared import(
     get_chi2 as get_chi2,
     combine_chi2 as combine_chi2,
+    ChiSquared as ChiSquared,
+    ChiSquaredList as ChiSquaredList,
 )
 
 from .functions import(
