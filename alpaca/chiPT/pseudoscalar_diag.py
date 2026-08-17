fix: normalization of chiPT couplings

alpaca/chiPT/pseudoscalar_diag.py
@@ -131,14 +131,14 @@ def cqA(couplings: ALPcouplings):
     couplings = couplings.translate('VA_below')
     cuA = couplings['cuA']
     cdA = couplings['cdA']
-    cqA = np.array([[cuA[0,0], 0, 0], [0, cdA[0,0], cdA[0,1]], [0, cdA[1,0], cdA[1,1]]])*2
+    cqA = np.array([[cuA[0,0], 0, 0], [0, cdA[0,0], cdA[0,1]], [0, cdA[1,0], cdA[1,1]]])*0.5
     return cqA
 
 def cqV(couplings: ALPcouplings):
     couplings = couplings.translate('VA_below')
     cuV = couplings['cuV']
     cdV = couplings['cdV']
-    cqV = np.array([[cuV[0,0], 0, 0], [0, cdV[0,0], cdV[0,1]], [0, cdV[1,0], cdV[1,1]]])*2
+    cqV = np.array([[cuV[0,0], 0, 0], [0, cdV[0,0], cdV[0,1]], [0, cdV[1,0], cdV[1,1]]])*0.5
     return cqV
 
 def cGA(couplings: ALPcouplings):